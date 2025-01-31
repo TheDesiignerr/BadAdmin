21e35b8e6de130488a2fe1d7c7cfd9a65dc70a02
XP-D <omartheedesigner@gmail.com>
2025-01-31 02:35:59 +0200

    Initial commit

---
+++ view/dashboard/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../style/main.css">
+    <link rel="stylesheet" href="../../style/dashboard.css">
+    <title>BadAdmin Systems</title>
+</head>
+<body>
+    <header>
+        <h1 class="headerTitle">BadAdmin Systems</h1>
+        <h2 class="headerSub">Leading Business Kindom</h2>
+    </header>
+
+    <main>
+        <div class="mainWrapper">
+            <div class="mainContainer">
+                <div class="panelBox">
+                    <h1 class="panelTitle">Sales</h1>
+                    <img src="../../assets/icons/sales.png" class="boxIcon">
+                </div>
+
+                <div class="panelBox">
+                    <h1 class="panelTitle">Vault</h1>
+                    <img src="../../assets/icons/vault.png" class="boxIcon">
+                    
+                </div>
+
+                <br>
+
+                <div class="panelBox">
+                    <h1 class="panelTitle">Inventory</h1>
+                    <img src="../../assets/icons/inv.png" class="boxIcon">
+
+                </div>
+
+                <div class="panelBox">
+                    <h1 class="panelTitle">Notes</h1>
+                    <img src="../../assets/icons/notes.png" class="boxIcon">
+
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
